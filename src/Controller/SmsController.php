9d7faa14df4767a97a37ee7773262b54b1a171ff
sakorn.s <sakorn.s@github.com>
2019-10-10 16:26:45 +0700

    Update SmsController.php

--- src/Controller/SmsController.php
+++ src/Controller/SmsController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Controller\AppController;
 use Cake\ORM\TableRegistry;
+
 /**
  * Sms Controller
  *
@@ -13,30 +14,40 @@ use Cake\ORM\TableRegistry;
 class SmsController extends AppController {
 
     public $Users = null;
-    
+
     public function createAndSendOtpPassword() {
-        
+        $result = ['result' => false, 'msg' => 'please use POST method.'];
+
         if ($this->request->is(['post'])) {
             $postData = $this->request->getData();
-            
-            if(!isset($postData['user_id'])){
-                
+
+            if (isset($postData['user_id']) && isset($postData['mobile']) && isset($postData['msg'])) {
+                $this->loadComponent('SMS');
+                $this->Users = TableRegistry::get('Users');
+                $user = $this->Users->find()->where(['Users.id' => $postData['user_id']])->first();
+
+                if (is_null($user)) {
+                    $result['msg'] = 'not found this user.';
+                } else {
+                    $otpNumber = mt_rand(100000, 999999);
+                    $user->otp = $otpNumber;
+                    $this->Users->save($user);
+
+                    $otpMsg = sprintf('Order Pang!,Your requested OTP is %d', $otpNumber);
+                    $this->SMS->send('OTP', $postData['mobile'], $otpMsg);
+                    $result = ['result' => true, 'msg' => 'success'];
+                }
+            } else {
+                $result['msg'] = 'please check require fields[user_id,mobile,msg]';
             }
-            if(!isset($postData['mobile'])){
-                
-            }
-            if(!isset($postData['msg'])){
-                
-            }
-            $this->loadComponent('SMS');
-            $this->Users = TableRegistry::get('Users');
-            
-            $otpNumber = mt_rand(100000,999999);
-            
-            $otpMsg = sprintf('Your requested OTP is %d',$otpNumber);
-            $this->SMS->send('OTP',$postData['mobile'],$otpMsg);
-            
         }
+
+        $json = json_encode($result, JSON_PRETTY_PRINT);
+        $this->set(compact('json'));
+        $this->set('_serialize', 'json');
+        $json = json_encode($result, JSON_PRETTY_PRINT);
+        $this->set(compact('json'));
+        $this->set('_serialize', 'json');
     }
 
 }
