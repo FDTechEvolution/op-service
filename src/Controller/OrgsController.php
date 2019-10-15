06f3da5977c843ad87b60f73923fdf1b60c9e584
artdata1 <48270726+artdata1@users.noreply.github.com>
2019-10-15 15:51:26 +0700

    15-10-62

--- src/Controller/OrgsController.php
+++ src/Controller/OrgsController.php
@@ -33,10 +33,10 @@ class OrgsController extends AppController
 
     public function getorgs($user = null){
         $user = $this->Users->find()->where(['id' => $user, 'org_id !=' => null])->toArray();
-        if(sizeof($user) != 0){
+        if(sizeof($user) == 0){
             $orgs = $this->Orgs->find()->where(['id' => $user->org_id])->toArray();
         }else{
-            $orgs = sizeof($user);
+            $orgs = '';
         }
 
         $json = json_encode($orgs,JSON_PRETTY_PRINT);
