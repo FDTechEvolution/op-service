8dc863bd300262b4b26739bbac31c26da592f5e1
sakorn.s <sakorn.s@github.com>
2019-10-15 09:54:54 +0700

    Update MyAuthenComponent.php

--- src/Controller/Component/MyAuthenComponent.php
+++ src/Controller/Component/MyAuthenComponent.php
@@ -27,12 +27,6 @@ class MyAuthenComponent extends Component {
     }
 
     public function hashPassword($password = '') {
-        $options = [
-            //'salt' => Security::getSalt(),
-            'salt' => mcrypt_create_iv(22, MCRYPT_DEV_URANDOM),
-            'cost' => 12,
-        ];
-        //$password = password_hash($password, PASSWORD_BCRYPT, $options);
         
         $password = (new DefaultPasswordHasher)->hash($password);
         
